1d00d4f5952d3bf74da0933759464865ff4b8dbc
Ilya Git <ilufst@protonmail.com>
2021-05-25 14:12:10 +0300

    edit get_code()

--- app/src/main/java/com/example/sms/Activity3.kt
+++ app/src/main/java/com/example/sms/Activity3.kt
@@ -3,17 +3,17 @@ package com.example.sms
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import org.jetbrains.anko.custom.async
 import org.jetbrains.anko.doAsync
 import java.net.URL
 
 class Activity3 : AppCompatActivity() {
 
     private lateinit var kod_text: TextView
+    private lateinit var button: Button
     private var number_text: TextView? = null
     private var api_key: String? = null
     var id: String = "_"
@@ -24,59 +24,52 @@ class Activity3 : AppCompatActivity() {
         setContentView(R.layout.activity_3)
         kod_text = findViewById(R.id.kod)
         number_text = findViewById(R.id.number)
+        button = findViewById(R.id.button)
 
         var map = intent.getStringExtra(MAP)
         api_key = map?.substringBefore("response:")
         var resp = map?.substringAfter("response:")
-        var response = resp?.substringAfterLast("ACCESS_NUMBER:")
+        var response = resp?.substringAfter("ACCESS_NUMBER:")
 
-        number = response?.substringAfterLast(":").toString()
-        id = response?.substringBeforeLast(":").toString()
+        number = response?.substringAfter(":").toString()
+        id = response?.substringBefore(":").toString()
 
-        val url = "https://sms-activate.ru/stubs/handler_api.php?api_key=$api_key&action=setStatus&status=1&id=$id"
 
-        Log.d("Number","$number - #id $id")
-        kod_text?.setText("чекаю ...")
-        number_text?.setText("$number")
 
     }
 
     override fun onResume() {
         super.onResume()
-        var kode = get_code(id)
-        kod_text.text = kode
-        kod_text.setOnClickListener {
-            kod_text.text = kode
+
+        // Зміна статуса на 1 - тобто готовий прийняти смс
+        val url_edit_status = "https://sms-activate.ru/stubs/handler_api.php?api_key=$api_key&action=setStatus&status=1&id=$id"
+        doAsync {
+            URL(url_edit_status)
         }
 
-    }
+        val url = "https://sms-activate.ru/stubs/handler_api.php?api_key=$api_key&action=setStatus&status=1&id=$id"
+            //Log.d("Number","$number - #id $id")
 
+        number_text?.text = "$number"
 
-    fun get_code(id:String): String? {
-        val url = "https://sms-activate.ru/stubs/handler_api.php?api_key=$api_key&action=getStatus&id=$id"
-        var response: String? = null
+        val url_get_code = "https://sms-activate.ru/stubs/handler_api.php?api_key=$api_key&action=getStatus&id=$id"
+        var code: String = "чекаю код.."
+        var response = ""
+        button.setOnClickListener() {
 
-        val job = GlobalScope.launch(Dispatchers.Main) {
-            while (true) {
-                val postOperation =
-                    async(Dispatchers.IO) { // <- extension on launch scope, launched in IO dispatcher
-                        // blocking I/O operation
-                        URL(url).readText()
-                    }
-                response =
-                    postOperation.await() // wait for result of I/O operation without blocking the main thread
-                    Log.d("Kod:" , response.toString())
-                if ("STATUS_OK:" in response!!) {
-                    response = response!!.substringBefore("STATUS_OK:").toString()
-                    kod_text.text = response
-                    break
-                   // return response?.substringAfter("STATUS_OK:")?.toInt()
+            doAsync {
+                URL(url_edit_status)
+                response = URL(url_get_code).readText()
+                if ("STATUS_OK:" in response) {
+                    code = response.substringAfter("STATUS_OK:").toString()
+                    kod_text.text = code
                 }
             }
+
         }
 
-        return response
     }
+
     companion object {
         const val MAP = "map"
     }
